d9fa010d78906aeeaa4e8d4d3bfeda419c14f95f
Nur Mutmainnah Rahim <18319006@std.stei.itb.ac.id>
2022-07-27 17:38:58 +0700

    Initial Commit

---
+++ EMODIA_NC2_INNA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EMODIA_NC2_INNA
+//
+//  Created by Nur Mutmainnah Rahim on 27/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
